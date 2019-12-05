74ab264dfb51ede4ea4a5fea8ff8f686e0c3aecb
Nick Petho <nick.petho@gmail.com>
2019-12-04 23:25:34 -0500

    Enemy Spawns
    -Added enemy spawn in script
    -Cleaned up treasure script

--- Assets/Scripts/EnemySpawns.cs
+++ Assets/Scripts/EnemySpawns.cs
@@ -2,33 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TreasureSpawns : MonoBehaviour
+public class EnemySpawns : MonoBehaviour
 {
-    public GameObject treasurePrefab;
-    public int spawnAmount = 5;
+    public GameObject enemyPrefab;
+    public int spawnAmount = 3;
 
     public Vector3[] positionArray = new[] {
-        new Vector3(-35.5f, 0f, -2.5f),
-        new Vector3(-25f, 0f, -2.5f),
-        new Vector3(-20f, 0f, -13f),
-        new Vector3(-5f, 0f, -18f),
-        new Vector3(35f, 0f, -3f),
-        new Vector3(35f, 0f, 2f),
-        new Vector3(15f, 0f, 17f),
-        new Vector3(-25f, 0f, 17f),
-        new Vector3(-20f, 0f, 7f),
-        new Vector3(5f, 0f, 12f)
+        new Vector3(-35f, 0f, 17f),
+        new Vector3(35f, 0f, 17f),
+        new Vector3(35f, 0f, -17f),
+        new Vector3(-35f, 0f, -18f),
+        new Vector3(-5f, 0f, 2f)
     };
 
     // Start is called before the first frame update
     void Start()
     {
         List<int> samples = SampleSpawns(spawnAmount);
-        
-        for(int i = 0; i < spawnAmount; i++)
+
+        for (int i = 0; i < spawnAmount; i++)
         {
             Vector3 spawnPos = positionArray[samples[i]];
-            Instantiate(treasurePrefab, spawnPos, treasurePrefab.transform.rotation);
+            Instantiate(enemyPrefab, spawnPos, enemyPrefab.transform.rotation);
             //Debug.Log(samples[i]);
         }
     }
@@ -36,7 +31,7 @@ public class TreasureSpawns : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     List<int> SampleSpawns(int num)
@@ -51,7 +46,7 @@ public class TreasureSpawns : MonoBehaviour
             do
             {
                 sample = Random.Range(0, positionArray.Length);
-            } while(set.Contains(sample));
+            } while (set.Contains(sample));
 
             set.Add(sample);
         }
